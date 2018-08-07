ed07fde1ea068af1fc3f826668d192e5cad98d0e
Noelle Foster <uufarmgirl@gmail.com>
2018-08-07 17:41:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function moveDodger(e) {
     if(e.which===LEFT_ARROW){
       moveDodgerLeft()
     }
-    if(e.which === RIGHT_ARROW){
+    else if(e.which === RIGHT_ARROW){
       moveDodgerRight()
     }
   })
@@ -151,7 +151,6 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
   function move(){
